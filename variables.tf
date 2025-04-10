26d810d263cd577831e9123f2fd7dcddecb4659b
varshajathan <48399281+varshajathan@users.noreply.github.com>
2025-04-10 16:50:05 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
 variable "region" {}
 variable "machine_type" {}
 variable "project_id" {}
+variable "gcp_credentials" {
+  description = "Base64 encoded GCP credentials JSON"
+  type        = string
+  sensitive   = true
+}
